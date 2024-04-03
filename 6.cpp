8c0dc497df3bbfeb0d214025b9121f0cf9a20ad7
vijay ahirwar <111403330+biusby@users.noreply.github.com>
2024-04-03 22:35:59 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+    int a,b,c,sum,A;
+
+    cout<<"enter three number";
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    A=sum%3;
+    cout<<A;
+
+return 0;
+
+
+
+}
